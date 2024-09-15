ab7839b8be6dc20d2b2047095f7d4a81b6caada9
youssi <zzz123zz12@naver.com>
2024-09-15 12:01:47 +0900

    하하하

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/providers/themeProviders";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="root"
+            enableSystem
+            disableTransitionOnChange
+          >
             {children}
+          </ThemeProvider>
       </body>
     </html>
   );
