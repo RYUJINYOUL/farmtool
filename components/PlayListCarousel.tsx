b8bf76146a90ecf4b044bfc48a8f545af86aad18
youssi <zzz123zz12@naver.com>
2025-04-03 05:47:12 +0900

    400px

--- components/PlayListCarousel.tsx
+++ components/PlayListCarousel.tsx
@@ -51,7 +51,7 @@ const PlayListCarousel: React.FC<PlayListCarouselProps> = ({
             return (
               <CarouselItem
                 key={index}
-                className="min-[425px]:basis-3/4 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+                className="min-[400px]:basis-3/4 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
               >
                 <PlayListCard playlist={playlist} />
               </CarouselItem>
